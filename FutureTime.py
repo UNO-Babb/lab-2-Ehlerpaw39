96983ad7d9eeca9bc26a19b289d0fe9d485f9616
Ehlerpaw39 <ehlerpaw@unomaha.edu>
2025-01-29 16:13:36 +0000

    futuretime

--- FutureTime.py
+++ FutureTime.py
@@ -9,14 +9,26 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 16
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
-  #Ask user for minutes
+  hours = input("Enter hours:")
+  hours = int(hours) #change from text to number 
+
+  futurehour = currentHour + hours
+  futurehour = futurehour % 24
+  print(futurehour)
+
+
+  futuremins = currentMinute + Mins
+  futuremin = futuremins % 60
+
+   #Ask user for minutes
+  Mins = input ("Enter mintues")
 
   #Calculate the time after the user-supplied time has passed.
 
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,8 +10,22 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answer = ["yes", "yes-definetely", "No", "Very Doubtful", "Ask Again later", "cannot Predict Now", "It is certain",
+            "Most likely", "I say no","outlook good", "outlook bad", "Not so sure",
+            "it is certain", "it is not certain", "you may rely on it", "you may not like it"]
   #Answer question randomly with one of the options from your earlier list.
+  question = input("Ask a question:")
+  num = random. random()
+  num = num * 1000
+  num = int(num)
+  num = num % 15
+  print(answer[num])
+  #print(answer[16])
+
+  response = random. choice(answer)
+  print(response)
+  print(random. choice(answer))
+  
 
 
 if __name__ == '__main__':
